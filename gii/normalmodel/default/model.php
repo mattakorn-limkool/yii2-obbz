c98b307c6fd8ffcd9f24bd2d4d7f1b2f2e8c2cf7
Mattakorn <mattakron@hotmail.com>
2017-03-17 05:16:51 +0700

    no message

--- gii/normalmodel/default/model.php
+++ gii/normalmodel/default/model.php
@@ -34,9 +34,9 @@ class <?= $className ?> extends <?= '\\'.$generator->ns.'\\base\\'.$className.'B
     public function attributeLabels(){
         return array_merge(parent::attributeLabels(),[
         <?php foreach ($labels as $name => $label): ?>
-            <?= "'$name' => " . $generator->generateString($label) . ",\n" ?>
+        <?= "'$name' => " . $generator->generateString($label) . ",\n" ?>
         <?php endforeach; ?>
-        ]);
+]);
     }
 	
 <?php foreach ($relations as $name => $relation): ?>
