1f858bf8c2d8ad83a77294136f84e87477d64998
Mattakorn <mattakron@hotmail.com>
2018-05-02 21:01:08 +0700

    no message

---
+++ widgets/GoogleChart.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * @author: Mattakorn Limkool
+ *
+ */
+
+namespace obbz\yii2\widgets;
+
+
+class GoogleChart extends \scotthuangzl\googlechart\GoogleChart
+{
+    const TYPE_PIE = 'PieChart';
+    const TYPE_LINE = 'LineChart';
+    const TYPE_COLUMN = 'ColumnChart';
+    const TYPE_BAR = 'BarChart';
+    const TYPE_AREA = 'AreaChart';
+    // have more//
+}
