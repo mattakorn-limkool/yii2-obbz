58d54ba43bceb1ed4c762611918e83705b223bd9
Mattakorn <mattakron@hotmail.com>
2018-12-21 22:56:45 +0700

    no message

---
+++ extensions/mpdf/HtmlPdf.php
@@ -0,0 +1,10 @@
+<?php
+namespace obbz\yii2\extensions\mpdf;
+/**
+ * @author: Mattakorn Limkool
+ *
+ */
+class HtmlPdf extends \kartik\mpdf\Pdf
+{
+
+}
--- utils/Thailand.php
+++ utils/Thailand.php
@@ -7,8 +7,26 @@
 namespace obbz\yii2\utils;
 
 use PA\ProvinceTh\Factory;
+use yii\helpers\ArrayHelper;
 
 class Thailand extends Factory
 {
+    public static function provinceList($language = 'th')
+    {
+        $provinces = self::province()->toArray();
+        $field = ($language == 'th') ? 'name_th' : 'name_en';
+        return ArrayHelper::map($provinces, 'id', $field);
+    }
 
+    public static function provinceValueById($id, $language = 'th'){
+        $provinces = self::province()->toArray();
+        $field = ($language == 'th') ? 'name_th' : 'name_en';
+        foreach($provinces as $province){
+            if($province['id'] == $id){
+                return $province[$field];
+            }
+        }
+
+        return null;
+    }
 }
