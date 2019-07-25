5730487b3caf4d47b256e124089fff9a50e419a0
Mattakorn <mattakron@hotmail.com>
2019-07-25 19:20:27 +0700

    update youtube config util

--- utils/YoutubeUtil.php
+++ utils/YoutubeUtil.php
@@ -6,6 +6,23 @@
 
 namespace obbz\yii2\utils;
 
+use yii\base\Component;
+
+class YoutubeUtilConfig extends Component
+{// for bootstrap
+    const ASPECT_RETIO_21_9 = 'embed-responsive-21by9';
+    const ASPECT_RETIO_16_9 = 'embed-responsive-16by9';
+    const ASPECT_RETIO_4_3 = 'embed-responsive-4by3';
+    const ASPECT_RETIO_1_1 = 'embed-responsive-1by1';
+
+    public $autoplay = false;
+    public $allowFullscreen = true;
+    public $allowAutoplay = true;
+
+    public $bootstrapContainerClass = 'embed-responsive';
+    public $bootstrapContainerRatio = self:: ASPECT_RETIO_16_9;
+}
+
 /**
  * Help to easy to embled url youtube
  * Class YoutubeUtil
@@ -15,16 +32,13 @@ class YoutubeUtil
 {
     const EMBLED_URL_PREFIX = 'https://www.youtube.com/embed/';
 
-    // for bootstrap
-    const ASPECT_RETIO_21_9 = 'embed-responsive-21by9';
-    const ASPECT_RETIO_16_9 = 'embed-responsive-16by9';
-    const ASPECT_RETIO_4_3 = 'embed-responsive-4by3';
-    const ASPECT_RETIO_1_1 = 'embed-responsive-1by1';
-
     const THUMB_QUALITY_LOW = 'sddefault.jpg';
     const THUMB_QUALITY_MEDIUM = 'mqdefault.jpg';
     const THUMB_QUALITY_HIGH = 'hqdefault.jpg';
     const THUMB_QUALITY_MAX = 'maxresdefault.jpg';
+
+
+
     /**
      * Get only Id from youtube
      *
@@ -47,25 +61,42 @@ class YoutubeUtil
         return isset($match[1]) ? $match[1] : '';
     }
 
+
     /**
      * using for embed VDO to iframe
      * @param $youtubeLink
+     * @param YoutubeUtilConfig $config
      * @return string
      */
-    public static function getEmbedUrl($youtubeLink){
+    public static function getEmbedUrl($youtubeLink, $config = null){
+        $suffix = '';
+        $config  = self::prepareConfig($config);
+        if($config->autoplay){
+            $suffix = '?autoplay=1';
+        }
+
         $id = self::getIdByUrl($youtubeLink);
-        return self::EMBLED_URL_PREFIX . $id;
+        return self::EMBLED_URL_PREFIX . $id . $suffix;
     }
 
     /**
      * @param $youtubeLink
-     * @param array $config
+     * @param YoutubeUtilConfig $config
      * @return string
      */
-    public static function getEmbedCode($youtubeLink, $config = ['allowfullscreen']){
+    public static function getEmbedCode($youtubeLink, $config = null){
         $embedUrl = self::getEmbedUrl($youtubeLink);
-        $confStr = self::parseConfig2String($config);
-        $result = '<iframe class="embed-responsive-item" src="'. $embedUrl .'" '. $confStr .'></iframe>';
+        $config  = self::prepareConfig($config);
+        $iframeAttr = '';
+        if($config->allowFullscreen){
+            $iframeAttr = 'allowfullscreen';
+        }
+
+        if($config->allowAutoplay){
+            $iframeAttr .= ' allow="autoplay"';
+        }
+
+        $result = '<iframe class="embed-responsive-item" src="'. $embedUrl .'" '. $iframeAttr .'></iframe>';
         return $result;
     }
 
@@ -76,24 +107,54 @@ class YoutubeUtil
      * @param array $youtubeConfig
      * @return string
      */
-    public static function getBootstrapEmbedCode($youtubeLink, $bootstrapConfig = ['embed-responsive-16by9'], $youtubeConfig = ['allowfullscreen']){
-        $iframe = self::getEmbedCode($youtubeLink, $youtubeConfig);
-        $confStr = self::parseConfig2String($bootstrapConfig);
-        return '<div class="embed-responsive '. $confStr .'">'. $iframe .'</div>';
+    public static function getBootstrapEmbedCode($youtubeLink, $config = null){
+        $config  = self::prepareConfig($config);
+        $iframe = self::getEmbedCode($youtubeLink, $config);
+
+        $containerClass = $config->bootstrapContainerClass . ' ' . $config->bootstrapContainerRatio;
+
+        return '<div class="'. $containerClass .'">'. $iframe .'</div>';
     }
 
-
-    public static function parseConfig2String($config){
-        $result = '';
-        foreach($config as $key => $value){
-            $result .= " " . $value;
+    /**
+     * @param null $config
+     * @return YoutubeUtilConfig
+     */
+    public static function prepareConfig($config = null){
+        if(!isset($config)){
+            $config = new YoutubeUtilConfig();
         }
-        return $result;
+        if($config->autoplay){
+            $config->allow_autoplay = true;
+        }
+
+        return $config;
     }
+
 
     /** for youtube thumbnail */
     public static function getDefaultThumbnail($youtubeLink, $quality = self::THUMB_QUALITY_MEDIUM){
         $id = self::getIdByUrl($youtubeLink);
         return "http://img.youtube.com/vi/{$id}/{$quality}";
     }
+
+
+    /**
+     * @deprecated
+     * @param $config
+     * @return string
+     */
+    public static function parseConfig2String($config){
+        $result = '';
+        foreach($config as $key => $value){
+            if(is_numeric($key)){
+                $result .= " " . $value;
+            }else{
+                $result .= " ".$key . ' = "'. $value .'"';
+            }
+
+        }
+        return $result;
+    }
+
 }
