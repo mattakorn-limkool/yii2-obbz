bac06cbfe7b8a414e60478a9bc87ce2170e2a9a6
mattakorn_limkool <mattakorn@gmail.com>
2020-05-03 18:44:13 +0700

    additional before after header grid

--- widgets/grid/CoreGridView.php
+++ widgets/grid/CoreGridView.php
@@ -21,6 +21,30 @@ class CoreGridView extends GridView
     public $additionalUrlParams = [];
     public $tableOptions = ['class' => 'core-grid table table-hover'];
 
+    /**
+     * @var array|string configuration of additional header table rows that will be rendered before the default grid
+     * header row. If set as a _string_, it will be displayed as is, without any HTML encoding. If set as an _array_,
+     * each row in this array corresponds to a HTML table row, where you can configure the columns with these properties:
+     * - `columns`: _array_, the header row columns configuration where you can set the following properties:
+     *    - `content`: _string_, the grid cell content for the column
+     *    - `tag`: _string_, the tag for rendering the grid cell. If not set, defaults to `th`.
+     *    - `options`: _array_, the HTML attributes for the grid cell
+     * - `options`: _array_, the HTML attributes for the table row
+     */
+    public $beforeHeader = [];
+
+    /**
+     * @var array|string configuration of additional header table rows that will be rendered after default grid header
+     * row. If set as a _string_, it will be displayed as is, without any HTML encoding. If set as an _array_, each
+     * row in this array corresponds to a HTML table row, where you can configure the columns with these properties:
+     * - `columns`: _array_, the header row columns configuration where you can set the following properties:
+     *    - `content`: _string_, the grid cell content for the column
+     *    - `tag`: _string_, the tag for rendering the grid cell. If not set, defaults to `th`.
+     *    - `options`: _array_, the HTML attributes for the grid cell
+     * - `options`: _array_, the HTML attributes for the table row
+     */
+    public $afterHeader = [];
+
 
     public function init()
     {
@@ -88,5 +112,64 @@ class CoreGridView extends GridView
         }
 
         return $result;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function renderTableHeader()
+    {
+        $cells = [];
+        foreach ($this->columns as $column) {
+            /* @var $column Column */
+            $cells[] = $column->renderHeaderCell();
+        }
+        $content = Html::tag('tr', implode('', $cells), $this->headerRowOptions);
+        if ($this->filterPosition === self::FILTER_POS_HEADER) {
+            $content = $this->renderFilters() . $content;
+        } elseif ($this->filterPosition === self::FILTER_POS_BODY) {
+            $content .= $this->renderFilters();
+        }
+
+        return "<thead>\n" .
+        $this->generateRows($this->beforeHeader) . "\n" .
+        $content . "\n" .
+        $this->generateRows($this->afterHeader) . "\n" .
+        '</thead>';
+    }
+
+    /**
+     * Generate HTML markup for additional table rows for header and/or footer.
+     *
+     * @param array|string $data the table rows configuration
+     *
+     * @return string
+     */
+    protected function generateRows($data)
+    {
+        if (empty($data)) {
+            return '';
+        }
+        if (is_string($data)) {
+            return $data;
+        }
+        $rows = '';
+        if (is_array($data)) {
+            foreach ($data as $row) {
+                if (empty($row['columns'])) {
+                    continue;
+                }
+                $rowOptions = ArrayHelper::getValue($row, 'options', []);
+                $rows .= Html::beginTag('tr', $rowOptions);
+                foreach ($row['columns'] as $col) {
+                    $colOptions = ArrayHelper::getValue($col, 'options', []);
+                    $colContent = ArrayHelper::getValue($col, 'content', '');
+                    $tag = ArrayHelper::getValue($col, 'tag', 'th');
+                    $rows .= "\t" . Html::tag($tag, $colContent, $colOptions) . "\n";
+                }
+                $rows .= Html::endTag('tr') . "\n";
+            }
+        }
+        return $rows;
     }
 }
